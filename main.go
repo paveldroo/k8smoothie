256bc83de69456f0999ffee81c646b832ce01f87
Pavel Druzhinin <paveldroo@gmail.com>
2025-08-24 16:14:32 +0300

    Add Pending status handling

--- main.go
+++ main.go
@@ -71,6 +71,8 @@ func main() {
 
 			switch p.Status.Phase {
 			case "Running", "Succeeded":
+			case "Pending":
+				fmt.Println("🤔 one of the pods in Pending status, if it takes too long - you'd better check it out")
 			default:
 				error = true
 			}
